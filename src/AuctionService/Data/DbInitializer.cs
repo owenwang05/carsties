b34c292a759628a624e9b7ab5c398bbeaecbcd6e
owen-wang-student <owang420@gmail.com>
2026-01-13 13:13:46 -0500

    configured docker container

---
+++ src/AuctionService/Data/DbInitializer.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace AuctionService.Data;
+
+public class DbInitializer
+{
+    // allows us to use it without initializing a new class
+    public static void InitDB(WebApplication app)
+    {
+        
+    }
+}
