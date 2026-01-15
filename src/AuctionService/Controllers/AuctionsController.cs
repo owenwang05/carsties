1a86c4ae495179526e10b99b63c1f28893e57fb8
owen-wang-student <owang420@gmail.com>
2026-01-15 12:05:44 -0500

    added create and update auction endpoints

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -1,6 +1,7 @@
 using System;
 using AuctionService.Data;
 using AuctionService.DTOs;
+using AuctionService.Entities;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -37,7 +38,50 @@ public class AuctionsController : ControllerBase
         var auction = await _context.Auctions
             .Include(x => x.Item)
             .FirstOrDefaultAsync(x => x.Id == id);
+
+        if (auction == null) return NotFound(); 
         
         return _mapper.Map<AuctionDto>(auction);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto auctionDto)
+    {
+        var auction = _mapper.Map<Auction>(auctionDto);
+
+        //TODO add current user as seller 
+        auction.Seller = "test"; 
+
+        _context.Auctions.Add(auction);
+        
+        var result = await _context.SaveChangesAsync() > 0;
+
+        if (!result) return BadRequest("Could not save changes into the DB");
+
+        return CreatedAtAction(nameof(GetAuctionById), new {auction.Id}, _mapper.Map<AuctionDto>(auction)); 
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDto updateAuctionDto)
+    {
+        var auction = await _context.Auctions
+            .Include(x => x.Item)
+            .FirstOrDefaultAsync(x => x.Id == id);
+
+        //TODO check seller == username
+
+        if (auction == null) return NotFound(); 
+
+        auction.Item.Make = updateAuctionDto.Make ?? auction.Item.Make; 
+        auction.Item.Model = updateAuctionDto.Model ?? auction.Item.Model; 
+        auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year; 
+        auction.Item.Color = updateAuctionDto.Color ?? auction.Item.Color; 
+        auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage; 
+
+        var result = await _context.SaveChangesAsync() > 0; 
+
+        if (result) return Ok(); 
+        
+        return BadRequest("Problem saving changes"); 
     }
 }
