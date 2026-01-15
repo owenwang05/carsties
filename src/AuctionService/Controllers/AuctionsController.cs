a7c1aed863134a73707e99345d1a818029a0c2bb
owen-wang-student <owang420@gmail.com>
2026-01-15 13:42:08 -0500

    completed controller

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -84,4 +84,20 @@ public class AuctionsController : ControllerBase
         
         return BadRequest("Problem saving changes"); 
     }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteAuction(Guid id)
+    {
+        var auction = await _context.Auctions.FindAsync(id);
+
+        if (auction == null) return NotFound(); 
+
+        _context.Auctions.Remove(auction);
+
+        var result = await _context.SaveChangesAsync() > 0;
+
+        if (result) return Ok(); 
+        
+        return BadRequest("Problem saving changes"); 
+    }
 }
