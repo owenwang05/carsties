e777ddb9afabf2842b4e3b233030b51109e29154
owen-wang-student <owang420@gmail.com>
2026-06-10 13:53:43 -0500

    created search endpoint

--- src/SearchService/Models/Item.cs
+++ src/SearchService/Models/Item.cs
@@ -7,9 +7,9 @@ public class Item : Entity
 {
     public int ReservePrice { get; set; }
     public required string Seller { get; set; }
-    public string? Winner { get; set; }
-    public int? SoldAmount { get; set; }
-    public int? CurrentHighBid { get; set; }
+    public string Winner { get; set; }
+    public int SoldAmount { get; set; }
+    public int CurrentHighBid { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
     public DateTime AuctionEnd { get; set; }
