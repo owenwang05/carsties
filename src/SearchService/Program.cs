3d2df78eb3eda1c1efeda61ff1c22475455efeed
owen-wang-student <owang420@gmail.com>
2026-06-10 14:53:49 -0500

    added request to auction service

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,9 +1,14 @@
+using System.Net;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService.Data;
+using SearchService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy()); 
 
 var app = builder.Build();
 
@@ -14,13 +19,23 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-try
+//ApplicationStarted is an event that fires after the web application has successfully started up.
+app.Lifetime.ApplicationStarted.Register(async () =>
 {
-    await DbInitializer.InitDb(app);
-}
-catch (Exception e)
-{
-    Console.WriteLine(e); 
-}
+    try
+    {
+        await DbInitializer.InitDb(app);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e); 
+    }
+});
 
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy() 
+    => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
